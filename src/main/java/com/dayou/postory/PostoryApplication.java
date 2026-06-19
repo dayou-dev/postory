da26bb668561e163dfad6b19152b0599c40a28e7
hyunwoo <jodogfoot@gmail.com>
2026-06-19 20:42:51 +0900

    first commit

---
+++ src/main/java/com/dayou/postory/PostoryApplication.java
@@ -0,0 +1,13 @@
+package com.dayou.postory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dayou/postory/PostoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayou.postory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
